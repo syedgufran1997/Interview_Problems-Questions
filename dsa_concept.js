0bfb9363f00b7da2a5bf04dedf85984071f5c166
syedgufran <syedgufran1997@gmail.com>
2024-02-22 02:53:47 +0530

    Data structures basics

---
+++ dsa_concept.js
@@ -0,0 +1,51 @@
+// Data Structures
+
+// It is a set of well defined instructions to solve a particular problem.
+
+// 1. Array methods
+// push , pop , shift, unshift
+// map , filter, sort, reduce, concat, slice, splice
+
+// 2. objects
+
+// 3. stacks DS - LIFO
+
+// 4. Queue DS - FIFO
+// Enqueue - Which add an element to the tail of the collection
+// Dequeue - Which removes an element from the head of the collection
+
+// 5. Linked List DS
+// It is a linear data structure that consists of Nodes,
+// Each node contains data and pointer to the next node in the list.
+// First node is called head and last is tail
+// Singly linked list and doubly linked list
+
+// 6. Hash Table
+// A hash table is a data structure that is used to store key-value pairs
+
+// 7. Tree Data Structure
+// A tree is hyrarchical Data structure that consists of nodes connected by edges
+// a tree is a non linear DS, tree will not contain any loops and cycles
+// Binary search tree (BST) - A binary tree is a tree data structure in which each
+// node has at most 2 children, they are refered to as left child and right child.
+
+// BST operations are
+// Insertion
+// search
+// DFS and BFS
+// Deletion
+
+// DFS - Depth First search
+// PreOrder traversal - start from root and left sub tree go back and right sub tree
+// InOrder traversal - start from left sub tree and go back and right then got back to root if no sub trees
+// Postorder traversal - sart from left and right siling then parent and root
+
+// BFS - Breadth First searh
+
+// Graph - A graph is non linear Data structure that consists of a finite number
+// of vertices connected by edges.
+
+// Directed graphs(one direction) - A graph in which the edges have a direction
+// Undirected Graphs(By directional) - A graph in which the edges are bidirectional and no arrows
+// use cases
+// google maps, social media sites
