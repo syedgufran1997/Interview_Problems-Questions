395bcd5c41dd2296f0962ae8b698f90ac424693a
syedgufran <syedgufran1997@gmail.com>
2024-05-20 19:34:47 +0530

    add walmart code

--- walmart.js
+++ walmart.js
@@ -1,8 +1,40 @@
-const graphList = [
+// const graphList = [
+//   {
+//     name: "script1",
+//     readingC: [{ file: "file a" }, { file: "file b" }],
+//   },
+
+//   script2[files[{ name: "file1", readSta: "" }]],
+// ];
+
+const graphLists = [
   {
     name: "script1",
-    readingC: [{ file: "file a" }, { file: "file b" }],
+    list: [
+      { key: "file a" },
+      { key: "file b" },
+      { key: "file c" },
+      { key: "file d" },
+    ],
   },
-
-  script2[files[{ name: "file1", readSta: "" }]],
+  {
+    name: "script2",
+    list: [
+      { key: "file a" },
+      { key: "file b" },
+      { key: "file c" },
+      { key: "file d" },
+    ],
+  },
+  {
+    name: "script3",
+    list: [
+      { key: "file a" },
+      { key: "file b" },
+      { key: "file c" },
+      { key: "file d" },
+    ],
+  },
 ];
+
+console.log("list", graphLists);
