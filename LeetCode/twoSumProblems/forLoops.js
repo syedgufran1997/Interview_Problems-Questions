ced0f3c0a78be66edbccd8c3f70a80c98d3ac555
syedgufran <syedgufran1997@gmail.com>
2024-08-09 22:49:42 +0530

    Day 3: two Sum problem using for loops

---
+++ LeetCode/twoSumProblems/forLoops.js
@@ -0,0 +1,20 @@
+// let nums = [2, 7, 11, 15];
+// let target = 9;
+
+// let nums = [3, 2, 4];
+// let nums = [3, 3];
+// let target = 6;
+
+const twoSum = function (nums, target) {
+  //   let sumArray = [];
+  //   for (let i = 0; i < nums.length; i++) {
+  //     for (let j = i + 1; j < nums.length; j++) {
+  //       if (target === nums[i] + nums[j]) {
+  //         sumArray.push([nums[i], nums[j]]);
+  //       }
+  //     }
+  //   }
+  //   return sumArray;
+};
+
+// console.log(twoSum(nums, target));
