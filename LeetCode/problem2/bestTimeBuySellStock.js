7de6d9eb3d5fb7876390d1f058bfdebcd454937f
syedgufran <syedgufran1997@gmail.com>
2024-08-11 14:10:55 +0530

    Day 5: LeetCode 2: Best time to buy sell stock problem with steps to solve

--- LeetCode/problem2/bestTimeBuySellStock.js
+++ LeetCode/problem2/bestTimeBuySellStock.js
@@ -1,9 +1,18 @@
-// let prices = [7, 1, 5, 3, 6, 4];
-// let prices = [7, 6, 4, 3, 1];
-let prices = [1, 2, 3, 4, 5];
-// Output: 5;
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+// Find and return the maximum profit you can achieve.
+
+let prices = [7, 1, 5, 3, 6, 4]; // Output = 7
+// let prices = [1, 2, 3, 4, 5]; //Output = 4
+// let prices = [7, 6, 4, 3, 1]; // Output = 0
 
 var maxProfit = function (prices) {
+  // check if length of array is even or odd
+  //    1. if array length is even split by 2.
+  //    2. Find the min value in both left and right.
+  //    3. Calculate the total profit of both left, rightand and Add
+  // 4. else if array length is odd then find the max profit
+
   let totalProfit = 0;
 
   if (prices.length % 2 === 0) {
