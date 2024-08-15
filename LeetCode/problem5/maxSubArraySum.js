9465891259625b684084a7f6e7b5d5402531a7c3
syedgufran <syedgufran1997@gmail.com>
2024-08-15 22:27:59 +0530

    Day 8: Leetcode 5 - Maximum sub array sum problem

--- LeetCode/problem5/maxSubArraySum.js
+++ LeetCode/problem5/maxSubArraySum.js
@@ -6,7 +6,20 @@ let nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]; // Output: 6;
 
 // using Kadane's algorithm
 function maxSubArray(nums) {
-  console.log(nums);
+  let currentSum = nums[0];
+  let maxSum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    currentSum = Math.max(nums[i], currentSum + nums[i]);
+    maxSum = Math.max(currentSum, maxSum);
+  }
+
+  return maxSum;
 }
 
 console.log(maxSubArray(nums));
+
+// It initializes maxSum and currentSum to the first element.
+// It iterates through the rest of the elements, updating currentSum to be the maximum of the current element or the sum of the current element and the previous currentSum.
+// It updates maxSum to be the maximum of the current maxSum and the current currentSum.
+// Finally, it returns maxSum, which contains the largest sum of any subarray in the input array.
