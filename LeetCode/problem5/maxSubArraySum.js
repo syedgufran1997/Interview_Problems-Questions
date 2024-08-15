d55176d895ef914ff6d75811426cbd8c92a0854c
syedgufran <syedgufran1997@gmail.com>
2024-08-15 22:04:02 +0530

    Day 8: Leetcode 5 - Maximum sub array sum problem

---
+++ LeetCode/problem5/maxSubArraySum.js
@@ -0,0 +1,12 @@
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+let nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]; // Output: 6;
+// let nums = [1]; // Output: 1;
+// let nums = [5, 4, -1, 7, 8]; // Output: 23;
+
+// using Kadane's algorithm
+function maxSubArray(nums) {
+  console.log(nums);
+}
+
+console.log(maxSubArray(nums));
