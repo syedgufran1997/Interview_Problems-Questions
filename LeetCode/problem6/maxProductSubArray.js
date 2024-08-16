48ee97652d78cb02d3700d52e7c45f62de22e3d6
syedgufran <syedgufran1997@gmail.com>
2024-08-16 20:21:58 +0530

    Day 8: Leetcode 6 - Find Maximum Product with sub array sum

--- LeetCode/problem6/maxProductSubArray.js
+++ LeetCode/problem6/maxProductSubArray.js
@@ -5,7 +5,28 @@ let nums = [2, 3, -2, 4]; // Output: 6;
 // let nums = [-2, 0, -1];   // Output: 0;
 
 function maxProduct(nums) {
-  console.log(nums);
+  if (nums.length === 0 && !nums) {
+    return 0;
+  }
+
+  let maxProduct = nums[0];
+  let minProduct = nums[0];
+  let result = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    let n = nums[i];
+
+    if (n < 0) {
+      [maxProduct, minProduct] = [minProduct, maxProduct];
+    }
+
+    maxProduct = Math.max(n, maxProduct * n);
+    minProduct = Math.min(n, minProduct * n);
+
+    result = Math.max(result, maxProduct);
+  }
+
+  return result;
 }
 
 console.log(maxProduct(nums));
