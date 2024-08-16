218dac8adf1053bf78c5f45ae9c69dd81ecc8e32
syedgufran <syedgufran1997@gmail.com>
2024-08-16 18:49:22 +0530

    Day 8: Leetcode 6

--- LeetCode/problem5/maxSubArraySum.js
+++ LeetCode/problem5/maxSubArraySum.js
@@ -1,6 +1,6 @@
 // Given an integer array nums, find the subarray with the largest sum, and return its sum.
 
-let nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]; // Output: 6;
+// let nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]; // Output: 6;
 // let nums = [1]; // Output: 1;
 // let nums = [5, 4, -1, 7, 8]; // Output: 23;
 
---
+++ LeetCode/problem6/maxProductSubArray.js
@@ -0,0 +1,11 @@
+// Given an integer array nums, find a subarray that has the largest product, and return the product.
+// The test cases are generated so that the answer will fit in a 32-bit integer.
+
+let nums = [2, 3, -2, 4]; // Output: 6;
+// let nums = [-2, 0, -1];   // Output: 0;
+
+function maxProduct(nums) {
+  console.log(nums);
+}
+
+console.log(maxProduct(nums));
