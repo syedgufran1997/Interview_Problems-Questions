d3ed971f9ab5918c5f1141a6804e25c6b4399449
syedgufran <syedgufran1997@gmail.com>
2024-08-27 00:43:46 +0530

    Day 14: mirafra technologies javascript problem with solution

--- Interviews/altemetric/index.js
+++ Interviews/altemetric/index.js
@@ -1,4 +1,18 @@
-// Controlled/uncontrolled components
+// React task
+// Show product quantity
+// where quantity can be increased and decreased numbers but number should not be negative
 
-let arr = [253, 3, 55, 55, 2, 6, 253, 253, 6, 1, 9, 1, 6];
+// React questions
+// What is Virtual DOM
+// how DOM updates in react
+// What is reconciliation in react
+// Controlled/uncontrolled components
+// what is react Portal
+
+// let arr = [253, 3, 55, 55, 2, 6, 253, 253, 6, 1, 9, 1, 6];
 // const outputArr = {253: 3,3: 1,55:2,2:1,6:3,1:2,9:1}
+
+// let result = arr.reduce(
+//   (acc, curr) => ((acc[curr] = acc[curr] + 1 || 1), acc),
+//   {}
+// );
