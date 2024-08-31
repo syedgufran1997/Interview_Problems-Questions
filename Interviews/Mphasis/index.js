3a267327344bb0a89570b0ae536d5a673d2386a2
syedgufran <syedgufran1997@gmail.com>
2024-08-31 10:51:08 +0530

    Day 17: Mphasis anagram problem

---
+++ Interviews/Mphasis/index.js
@@ -0,0 +1,6 @@
+// Fetch api and show results in react
+// Javascript Anagram problem
+
+// check if all the values in str1 is in str2 then print true or false
+// const str1 = 'abac'
+// const str2 = 'acba'
---
+++ Interviews/impactAnalysis/index.js
@@ -0,0 +1,23 @@
+// Dynamic forms in Ract js
+
+// Question
+// Create a React compenent with input, select fields
+// Component should take 2 props (config, callback)
+// On submit form it should call callback and log results in console.
+
+// const result = {
+//   name: "abc",
+//   age: 123,
+//   grade: "two",
+// };
+
+// const DATA = [
+//   { id: "name", type: "text", label: "Name" },
+//   { id: "age", type: "number", label: "Age" },
+//   {
+//     id: "grade",
+//     type: "select",
+//     label: "Grade",
+//     options: ["one", "two", "three"],
+//   },
+// ];
