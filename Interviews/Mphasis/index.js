ea72d5e045615d34d47a4ea5ce174295d66a8e36
syedgufran <syedgufran1997@gmail.com>
2024-08-31 18:34:42 +0530

    Day 17: Mphasis anagram problem solved

--- Interviews/Mphasis/index.js
+++ Interviews/Mphasis/index.js
@@ -2,5 +2,21 @@
 // Javascript Anagram problem
 
 // check if all the values in str1 is in str2 then print true or false
-// const str1 = 'abac'
-// const str2 = 'acba'
+const str1 = "abac";
+const str2 = "acba";
+
+function areValuesIn(str1, str2) {
+  // Convert str2 into an array to allow removal of characters
+  let str2Array = str2.split("");
+  for (let char of str1) {
+    const index = str2Array.indexOf(char);
+    if (index === -1) {
+      // Character from str1 is not found in str2
+      return false;
+    }
+    // Remove the character from str2Array to avoid duplicate counting
+    str2Array.splice(index, 1);
+  }
+  return true;
+}
+console.log(areValuesIn(str1, str2));
