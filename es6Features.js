06e109e378ac4505b1021afcd7bf48b895df6203
syedgufran <syedgufran1997@gmail.com>
2024-09-04 14:06:28 +0530

    New Es^ Fetures in javascript

---
+++ es6Features.js
@@ -0,0 +1,14 @@
+// 1. let and  const
+// 2. Arrow functions
+// 3. Template Literals
+// 4. Default Parameters -
+// 5. Desttructuring Assignments - Array, Object desctructuring
+// 6. Rest and Spread Operatiors
+// 7. Classes
+// 8. Modules
+// 9. Promises
+// 10. Enhanced Object Literals
+// 11. Iterators and for of loop
+// 12. Map and Set
+// 13. Symbol
+// 14. Generator Functions - using yield and next()
