b7761616193607bb441097024759978ead0c3d52
syedgufran <syedgufran1997@gmail.com>
2024-09-07 16:20:32 +0530

    MindSprint technical problem question with solution

--- Interviews/mindSprint/index.js
+++ Interviews/mindSprint/index.js
@@ -1,1 +1,28 @@
 // L1 Technical Round 7th september 2024 - 3pm to 3:45pm
+// SIVA SAILAM RAMALINGAM
+
+// What is React, redux, Virtual Dom,
+// what is Access and refresh token
+// How will we know if the access token is expired and how will it set with refresh token in react application
+
+// let inputArray = [1, 2, 3, 4, 5]; //output = [1,3,6,10,15]
+// function addSum(inputArray) {
+//   let outputArray = [];
+//   let sum = 0;
+//   for (let i = 0; i < inputArray.length; i++) {
+//     sum = sum + inputArray[i];
+//     outputArray.push(sum);
+//   }
+//   return outputArray;
+// }
+// console.log(addSum(inputArray)); // output = [1,3,6,10,15]
+
+// let inputArray = [1, 2, 3, 4, 5]; //output = 15
+// function sumN(inputArray) {
+//   let sum = 0;
+//   for (let i = 0; i < inputArray.length; i++) {
+//     sum = sum += inputArray[i];
+//   }
+//   return sum;
+// }
+// console.log(sumN(inputArray)); // 15
