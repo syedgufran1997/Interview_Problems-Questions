4eb414c60b6d62cf671259a1ec7d99ce6a4d54bf
syedgufran <syedgufran1997@gmail.com>
2024-09-07 13:54:13 +0530

    Javascript logical thinking questions with solution

---
+++ jsLogicalThinking.js
@@ -0,0 +1,51 @@
+// const a = 12;
+// console.log(a ** 12);
+
+// function test(){
+//     console.log(test.abc)
+// }
+// console.log(test());  // undefined
+// test.abc = 400;
+// test.abc = 600;
+// console.log(test());  // undefined
+
+// console.log({} == {}); // false
+// console.log({} === {});  // false
+
+// let a = {name: 'syed'}
+// let b = {
+//     ...a
+// }
+// b.name = 'Gufran'
+// console.log(a.name); // syed
+
+// let a = 10;
+// let b = new Number(10);
+// let c = 10;
+// console.log(a === b );
+// console.log(b === c);
+
+// function test(record){
+//     if(record == {age: 18}){
+//         console.log("youre an adult");
+//     }else if(record == {age: 28}){
+//         console.log("you're still an adult");
+//     }else{
+//         console.log('no record');
+//     }
+// }
+// console.log(test({age: 28}))
+
+// console.log(+true); // 1
+// console.log(!'abcd'); // false
+
+// var a = {};
+// var b = {key: 'b'}
+// var c = { key: "c" };
+// a[b]=600;
+// b[c]=700;
+// // b[b] = 400;
+// console.log(a[c]); // 600
+// console.log(a[b]); // 600
+// console.log(b[b]); // 700
+// console.log(b[c]); // 700
