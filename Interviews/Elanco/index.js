356b4517229d9bc8365c86a910001999ca7dceeb
syedgufran <syedgufran1997@gmail.com>
2024-09-09 21:16:44 +0530

    Elanco R2 interview Questions

--- Interviews/Elanco/index.js
+++ Interviews/Elanco/index.js
@@ -1,1 +1,15 @@
 // Role: Software Engineer (R&D) R1 05/Sept/2024 2PM
+// All basic and Advanced React and Javascript Questions
+
+// Role: Software Engineer (R&D) R2 09/Sept/2024 5PM
+// Strong understanding for Fullstack
+// Passionate
+// Technical knowledge
+// Api, Devops Knowledge
+// Issue tackling
+// Situational Questions
+// Questions on Node
+// How to write a GetData Api sing Node js and Express
+
+// Tech Lead
+// No Technical Assessment or Coding
