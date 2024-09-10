e1fa1e89861e62c8e9913b8c0039df634f155962
syedgufran <syedgufran1997@gmail.com>
2024-09-10 21:17:05 +0530

    Indum interview questions

---
+++ Interviews/indiumSoft/index.js
@@ -0,0 +1,26 @@
+// L1 Technical Discussion_Indium Software - 10 September 2024 - 1pm to 2pm
+
+// Javascript Questions
+// 1. What is Javascript?
+// 2. What is Currying in javascript?
+// 3. What is Coercion and it's types in javascript?
+// 4. what is PassByValue and PassByReference?
+
+// Typesript Questions
+// 1. What is Typescript
+// 2. Why do we use typescript
+// 3. How Typescript is different form Javascript
+// 4. Is All javascript is typescript
+// 5. What are Types, Interface and enums in js
+// 6. Diff btn any and Unknown and null in typescript
+// 7. How can we handle null exceptions for data in typescript
+// 8. Why/when/what is Void in TS
+// 9. How do you define type for Nested object
+
+// Next js Questions
+// 1. What is Next js?
+// 2. How Next js is different from Next js?
+// 3. How will impress someone to use Next JS?
+// 4. How to pass data between pages in Next js?
+// 5. Diff btn SSG and SSR in Next js?
+// 6. What are the New features in next js apart from SSR?
