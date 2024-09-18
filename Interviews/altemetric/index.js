e336a5fb7fd650b02641f2b0ca6cb887e86587df
syedgufran <syedgufran1997@gmail.com>
2024-09-18 23:39:47 +0530

    Altemetric l2 javascript question

--- Interviews/altemetric/index.js
+++ Interviews/altemetric/index.js
@@ -20,3 +20,24 @@
 //   (acc, curr) => ((acc[curr] = acc[curr] + 1 || 1), acc),
 //   {}
 // );
+
+// Altemetric L2 round Assesment javascript question
+// Write a function
+// findDuplicates(arr)
+// that takes in an array of integers as an argument and returns an array of all the integers that
+// occur more than once in the input array. The returned array should not have any repeated values,
+//  and can be in any order.
+
+// Example:
+
+// const arr = [1, 2, 3, 2, 4, 5, 2, 6, 3, 7, 8, 6];
+// // console.log(findDuplicates(arr)); // should return [2, 3, 6]
+
+// function duplicat(arr) {
+//   let result = [];
+//   let output = [];
+
+//   return output;
+// }
+
+// console.log(duplicat(arr));
