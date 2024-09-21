f2f757247e6769d75000684423bbada154cb2baf
syedgufran <syedgufran1997@gmail.com>
2024-09-21 12:54:03 +0530

    Intuit questions

---
+++ Interviews/intuit/index.js
@@ -0,0 +1,20 @@
+// Altemetric L2 round Assesment javascript question
+// Write a function
+// findDuplicates(arr)
+// that takes in an array of integers as an argument and returns an array of all the integers that
+// occur more than once in the input array. The returned array should not have any repeated values,
+//  and can be in any order.
+
+// Example:
+
+// const arr = [1, 2, 3, 2, 4, 5, 2, 6, 3, 7, 8, 6];
+// // console.log(findDuplicates(arr)); // should return [2, 3, 6]
+
+// function duplicat(arr) {
+//   let result = [];
+//   let output = [];
+
+//   return output;
+// }
+
+// console.log(duplicat(arr));
