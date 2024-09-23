6ca39f4aa10c3e6aa6df2c4c50bf985cac1c1708
syedgufran <syedgufran1997@gmail.com>
2024-09-23 23:48:39 +0530

    Vanilla JS practice todo app add complete todo functionality

--- VanillaJavascript/TodoVanillaJS/index.js
+++ VanillaJavascript/TodoVanillaJS/index.js
@@ -6,25 +6,32 @@ document.addEventListener("DOMContentLoaded", () => {
   function createTaskList(task) {
     const li = document.createElement("li");
     li.textContent = task;
+
+    // Remove todo Button
     const removeButton = document.createElement("button");
     removeButton.textContent = "Del";
     removeButton.className = "removeButton";
-
     removeButton.onclick = () => {
       li.remove();
     };
-
     li.append(removeButton);
+
+    // Complete Todo Button
+    const completeTodoButton = document.createElement("button");
+    completeTodoButton.textContent = "done";
+    completeTodoButton.className = "completeTodoButton";
+    completeTodoButton.onclick = () => {
+      li.className = "completeTodo";
+    };
+    li.append(completeTodoButton);
     return li;
   }
 
   addTodoButton.addEventListener("click", () => {
     const task = inputText.value.trim();
-
     if (task) {
       const taskElement = createTaskList(task);
       todoList.append(taskElement);
-
       inputText.value = "";
     }
   });
@@ -35,3 +42,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Fetch APi method using Vanilla JS
+//   let ApiData = [];
+//   function fetchApi() {
+//     const url = "https://dummyjson.com/products";
+//     fetch(url)
+//       .then((res) => res.json())
+//       .then((res) => (ApiData = res.products))
+//       .catch((error) => {
+//         throw new Error(error.message);
+//       });
+//   }
+//   fetchApi();
+//   console.log(ApiData);
