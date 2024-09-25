cd88679acbc8c3022b602391845459414d7f385c
syedgufran <syedgufran1997@gmail.com>
2024-09-25 13:16:32 +0530

    Build App using Vanilla JS Fetch API

--- VanillaJavascript/fetchApiVanillaJS/index.js
+++ VanillaJavascript/fetchApiVanillaJS/index.js
@@ -1,17 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  function fetchApi() {
-    const url = "https://dummyjson.com/products";
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => displayApiData(res))
-      .catch((error) => {
-        throw new Error(error.message);
-      });
+  async function fetchApi() {
+    try {
+      const response = await fetch("https://dummyjson.com/products");
+      const data = await response.json();
+      if (data) {
+        displayApiData(data);
+      }
+    } catch (error) {
+      console.log("Error fetching the products", error);
+    }
   }
   fetchApi();
 
   function displayApiData(data) {
-    const products = data.products[0];
-    console.log(products);
+    const Products = data.products;
+    console.log(Products);
+    const productList = document.getElementById("productList");
+
+    Products?.forEach((product) => {
+      const productCard = document.createElement("div");
+      productCard.className = "productCard";
+      productList.appendChild(productCard);
+
+      const productImage = document.createElement("img");
+      productImage.src = product.images[0];
+      productImage.className = "productImage";
+      productCard.appendChild(productImage);
+
+      const heading = document.createElement("h3");
+      heading.textContent = `${product.title}`;
+      productCard.appendChild(heading);
+
+      const description = document.createElement("p");
+      description.textContent = product.description;
+      productCard.appendChild(description);
+    });
   }
 });
