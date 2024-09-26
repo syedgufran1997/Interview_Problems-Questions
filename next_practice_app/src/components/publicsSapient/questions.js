8aacf9354c588cddd5f319f21f040d07c69609ec
syedgufran <syedgufran1997@gmail.com>
2024-09-26 20:06:37 +0530

    Publcis sapient interview questions

---
+++ next_practice_app/src/components/publicsSapient/questions.js
@@ -0,0 +1,28 @@
+// What is semantic html elements
+// What are advantages of using semantic HTML elements.
+// What is eslint explain any 3 eslint rules
+
+// Vanilla js
+// What is webpack and alternatives to that.
+// What is mocks and slimming
+// Writing the code in import and export way in structured format.
+// Write vanilla javascript in precise way.
+// Create a dropdown and work on that event listeners.
+
+// What is typescript
+// What is ts.config.js
+// Why do we use ts.config.js
+// Explain the reason for using all files in typescript
+// What is webpack
+// What is generics , types and interfaces, tuples
+
+// Unit test cases
+// Writing a test case for fetch api.
+// Writing a test case for buttons.
+
+// gitHooks
+// What is turbo repo
+// What is mono repo
+// Have you ever server and
+// what is web servers
+// What is Promise.all how it works
