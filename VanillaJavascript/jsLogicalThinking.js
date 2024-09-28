45238cb4748a7d168583d5066783fba25a579c12
syedgufran <syedgufran1997@gmail.com>
2024-09-28 15:09:44 +0530

    javascript logical thinking questions

--- VanillaJavascript/jsLogicalThinking.js
+++ VanillaJavascript/jsLogicalThinking.js
@@ -53,3 +53,39 @@
 // console.log(a[b]); // 600
 // console.log(b[b]); // 700
 // console.log(b[c]); // 700
+
+// // Round 1
+// const arr = [1, 2, 3, 4, 5];
+// for (var i = 0; i < arr.length; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 1000);
+// }
+// Answer is 5
+
+// let x = [1, 2, 3];
+// let y = [1, 2, 3];
+// let z = y;
+// console.log(x == y); // false
+// console.log(x === y); // false
+// console.log(z == y); // true
+// console.log(z == x); // false
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [...arr1];
+// arr2.push(4);
+// console.log(arr1);
+// console.log(arr2);
+
+// const person = {
+//   name: "John",
+//   age: 30,
+// };
+// Object.freeze(person);
+// person.age = 40;
+// console.log(person.age);
+// ⦁	A: 30
+// ⦁	B: 40
+// ⦁	C: TypeError
+// ⦁	D: ReferenceError
+// ANSWER is = 30 because freeze it freezes all the values and original values doest not change
