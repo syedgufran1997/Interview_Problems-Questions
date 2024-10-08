2248b42f0df965403b2cad598a08adbad969e134
syedgufran <syedgufran1997@gmail.com>
2024-10-08 16:07:07 +0530

    Pure Soft technologies Interview questions

---
+++ VanillaJavascript/puresoftTechnologies/index.js
@@ -0,0 +1,25 @@
+// new Promise((resolve)=>{
+// console.log(1);
+// resolve(2);
+// }).then(result=> console.log(result));
+// console.log(3);
+
+// 3 , 1, 2 by Syed Gufran
+
+// ----------------------------
+// var x=5;   function foo(){     console.lo... by Abhishek Bhardwaj
+// Abhishek Bhardwaj
+// 15:41
+// var x=5;
+
+// function foo(){
+//     console.log(x);// output
+//     console.log(y);//output
+//     var x=10;
+//     let y=2;
+// }
+// foo();
+// x = 5, Y = undefined by Syed Gufran
+
+// ----------------------------
+// diff btw React.Memo and useMemo
