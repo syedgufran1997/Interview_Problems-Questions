c57aebb2552db45da770fb31772f3a1971047907
syedgufran <syedgufran1997@gmail.com>
2024-10-10 14:54:59 +0530

    Turing questions

---
+++ Interviews/turing/index.js
@@ -0,0 +1,16 @@
+// How do you handle asynchronous operations in Node.js? Can you discuss the pros and cons of using callbacks, promises, and async/await for managing asynchronous code?
+// How do you manage environment variables in a Node.js application? Can you discuss best practices for securely handling sensitive information like API keys and database credentials?
+
+// Explain the concept of variable scoping in Python and provide examples of how global and local variables are accessed within different scopes.
+// Discuss the differences between shallow copy and deep copy in Python and provide scenarios where each is useful.
+// Explain the concept of generators in Python and provide an example of how they can be used to efficiently process large datasets.
+
+/**
+ * Write a function that takes an array of integers and an integer k, and returns the maximum sum of any contiguous subarray of length k.
+ * array => [2, 3, 4, 10, 9, 5, 1]
+ * k = 2
+ * Output => 19
+ */
+
+// let array = [2, 3, 4, 10, 9, 5, 1];
+// let k = 2;
