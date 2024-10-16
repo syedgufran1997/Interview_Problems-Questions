fe4c353ed701324ea32de5373812fe61dde37013
syedgufran <syedgufran1997@gmail.com>
2024-10-16 12:22:13 +0530

    Wissen technologies questions

---
+++ Interviews/wissenTechnologies/index.js
@@ -0,0 +1,19 @@
+// Answer the questions in CRISP and CLEAR way in one sentence
+// 1. Difference between CSR and SSR
+// 2. Difference between Apollo Client and Redux
+// 3. What and How javascript Event loop works
+
+// What will be the result for the code and how will the code prints explain using event loop concept
+// for (var i = 0; i <= 5; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
+
+// output 6 prints 6 times
+// 6 6 6 6 6 6
+
+// Using var inside ForLoop print from 0 to 5 and it should wait for 2 miliseconds after each number prints
+// output 1 wait 2 wait 3 wait 4 wait 5
+
+for (var i = 0; i < 5; i++) {}
--- VanillaJavascript/practicePage.js
+++ VanillaJavascript/practicePage.js
@@ -20,3 +20,18 @@
 //   var x = 100;
 //   console.log(x);
 // }
+
+// for (let i = 0; i <= 5; i++) {
+//   (function (i) {
+//     setTimeout(() => {
+//       console.log(i);
+//     }, 4000);
+//   })(i);
+// }
+for (var i = 1; i <= 5; i++) {
+  (function (i) {
+    setTimeout(function () {
+      console.log(i);
+    }, i * 2000); // Delay increases by 2 milliseconds for each iteration
+  })(i);
+}
