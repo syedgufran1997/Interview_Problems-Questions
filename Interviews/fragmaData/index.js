9893aa83aa07985d4bf241c1d81e99473cf168af
syedgufran <syedgufran1997@gmail.com>
2024-11-26 18:49:58 +0530

    Fragma Datasystems interview questions

--- Interviews/fragmaData/index.js
+++ Interviews/fragmaData/index.js
@@ -1,18 +1,16 @@
 // 26/11/2024 Fragma data systems React Ui interview
 
-// export default function App() {
-//   console.log(1 < 2 < 3);
-//   console.log(3 > 1 > 2);
+// Is javascript syn or async
+// event propogation in javascript
+// How to do you think javscript behaves by default by event bubling or capturing and expalin with example
+// diff between controlled and pure components
+// diff between UseMemo and React Memo
+// In javascript how the code compiles from (left to right) or (right to left )
 
-//   [] == [];
-//   false == 0;
-//   3 + 4 + "5" + 6 + "7" + 8;
-//   1 + +"2" + "2";
-
-//   return (
-//     <div className="App">
-//       <h1>Hello CodeSandbox</h1>
-//       <h2>Start editing to see some magic happen!</h2>
-//     </div>
-//   );
-// }
+console.log(1 < 2 < 3);
+console.log(3 > 2 > 1);
+console.log([] == []);
+console.log(false == 0);
+console.log(true == 0);
+console.log(3 + 4 + "5" + 6 + "7" + 8);
+console.log(1 + +"2" + "2");
