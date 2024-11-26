9e984804b8a3be84f326b0ae8b06924e4a17ee3d
syedgufran <syedgufran1997@gmail.com>
2024-11-26 18:42:19 +0530

    Fragma Datasystems interview questions

---
+++ Interviews/fragmaData/index.js
@@ -0,0 +1,18 @@
+// 26/11/2024 Fragma data systems React Ui interview
+
+// export default function App() {
+//   console.log(1 < 2 < 3);
+//   console.log(3 > 1 > 2);
+
+//   [] == [];
+//   false == 0;
+//   3 + 4 + "5" + 6 + "7" + 8;
+//   1 + +"2" + "2";
+
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//     </div>
+//   );
+// }
