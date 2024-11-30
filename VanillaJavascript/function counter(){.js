6dd96816696e6d2969cdf3533282936e6b84b72f
syedgufran <syedgufran1997@gmail.com>
2024-11-30 14:06:59 +0530

    counter function

--- VanillaJavascript/function counter(){.js
+++ VanillaJavascript/function counter(){.js
@@ -1,6 +1,11 @@
 function counter() {
   let count = 0;
-  return function () {
+  return function a() {
     return count++;
   };
 }
+
+let counter1 = counter();
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
