b3ee1f59fcbe2056ff9f74c55ae5083d7f3d935b
syedgufran <syedgufran1997@gmail.com>
2025-01-13 20:28:54 +0530

    slate ai interview questions soled

--- react_app_interviews/src/interviews/slate.ai/index.js
+++ react_app_interviews/src/interviews/slate.ai/index.js
@@ -9,46 +9,30 @@ import "./styles.css";
 
 export default function App() {
   const [count, setCount] = useState(0);
-  // const [progress, setProgress] = useState(false);
 
   const handleIncreaseClick = () => {
     if (count < 100) {
-      setCount(count + 1);
+      setCount(count + 25);
     }
   };
 
   const handleDecreaseClick = () => {
     if (count > 0) {
-      setCount(count - 1);
+      setCount(count - 25);
     }
-  };
-
-  const handleProgressbar = () => {
-    setCount(100);
   };
 
   return (
     <>
       <div className="App">
         <button onClick={handleIncreaseClick}>Increase</button>
-        {/* <input type="number" value={count} placeholder="Count" /> */}
         {count}
         <button onClick={handleDecreaseClick}>Decrease</button>
       </div>
 
       <div className="mainProgressWrap">
-        <div
-          // className="progressBarbefore"
-          style={{
-            backgroundColor: "red",
-            height: "49px",
-            borderRadius: "40px",
-            width: `${count}%`,
-          }}
-        ></div>
+        <div className="progressBarbefore" style={{ width: `${count}%` }}></div>
       </div>
-
-      <button onClick={handleIncreaseClick}>Start</button>
     </>
   );
 }
